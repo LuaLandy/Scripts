fa6654fdd0a9766d69321398aee6cf3ef8769c98
Zephyr <wbrololv7@gmail.com>
2025-07-19 15:27:57 +0200

    Create Example.lua

---
+++ NotificationSystemV2/Example.lua
@@ -0,0 +1,5 @@
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaLandy/Scripts/refs/heads/main/NotificationSystemV2/Main.lua"))()
+
+Notification("Title", "Notification without sound", 5)
+wait(1)
+Notification("Title", "Notification with sound.", 5, 4590657391)
