b4b6ad8598cd5141ef9e49808ea8d6477e40776a
Zephyr <wbrololv7@gmail.com>
2025-07-23 23:12:31 +0200

    Delete ZephyrAdmin/main.lua

--- ZephyrAdmin/main.lua
+++
@@ -1,8 +0,0 @@
-if not game:IsLoaded() then
-    game.Loaded:Wait()
-end
-
-local script = game:HttpGet("https://raw.githubusercontent.com/LuaLandy/Scripts/refs/heads/main/ZephyrAdmin/source.lua")
-
-queue_on_teleport(script)
-loadstring(script)()
