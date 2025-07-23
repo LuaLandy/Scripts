abe7c43c0e05dac270229da76f5a37c7d706627a
Zephyr <wbrololv7@gmail.com>
2025-07-23 23:11:31 +0200

    Update main.lua

--- ZephyrAdmin/main.lua
+++ ZephyrAdmin/main.lua
@@ -2,9 +2,7 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-local script = [[
-https://raw.githubusercontent.com/LuaLandy/Scripts/refs/heads/main/ZephyrAdmin/source.lua
-]]
+local script = game:HttpGet("https://raw.githubusercontent.com/LuaLandy/Scripts/refs/heads/main/ZephyrAdmin/source.lua")
 
 queue_on_teleport(script)
 loadstring(script)()
