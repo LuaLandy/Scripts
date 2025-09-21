b2784c4f77bf7acbb0720e072e9e43ecb6bfdaf1
Zephyr <wbrololv7@gmail.com>
2025-09-21 06:27:37 +0200

    Update Example.lua

--- NotificationSystemV3/Example.lua
+++ NotificationSystemV3/Example.lua
@@ -1,5 +1,6 @@
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaLandy/Scripts/refs/heads/main/NotificationSystemV3/Main.lua"))()
+local NotificationSystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaLandy/Scripts/refs/heads/main/NotificationSystemV3/Main.lua"))()
+local Notification = NotificationSystem.Notification
 
 Notification("Notification without sound", 5)
 wait(1)
-Notification("Notification with sound", 5, SoundID)
+Notification("Notification with sound", 5, 91882565)
