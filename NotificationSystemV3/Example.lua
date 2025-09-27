fe508f63ed931dd469b04cbd45a14ade37023458
Zephyr <wbrololv7@gmail.com>
2025-09-27 07:54:55 +0200

    Update Example.lua

--- NotificationSystemV3/Example.lua
+++ NotificationSystemV3/Example.lua
@@ -1,6 +1,10 @@
 local NotificationSystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaLandy/Scripts/refs/heads/main/NotificationSystemV3/Main.lua"))()
 local Notification = NotificationSystem.Notification
 
-Notification("Notification without sound", 5)
+Notification("Notification without sound", 5, nil, false)
 wait(1)
-Notification("Notification with sound", 5, 91882565)
+Notification("Notification with sound", 5, 91882565, false)
+wait(1)
+Notification("RGB Notification", 5, nil, true)
+wait(1)
+Notification("Custom Bar Color", 5, nil, false, Color3.fromRGB(0, 255, 0))
